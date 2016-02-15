96aa2ef41343d47cecbe4bd77a985ab5c3c95158
akito <akito@localhost.localdomain>
2016-02-15 10:38:41 +0700

    add assets images and fonts, add enqueue script in wp for noxus styles

--- lib/setup.php
+++ lib/setup.php
@@ -96,6 +96,7 @@ function display_sidebar() {
  */
 function assets() {
   wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  wp_enqueue_style('noxus/css', Assets\asset_path('styles/style.css'), false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
