9d4a59b0ccbe360590047dd24fe38e2acdaf9dcd
akito <akito@localhost.localdomain>
2016-02-17 09:47:30 +0700

    modified footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,12 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="container-fluid">
+    <div id="footer-top"></div>
+
+    <ul class="nav" id="nav-footer">
+        <li><a href="contact.html"><i class="fa fa-comment"></i> Kontak Kami</a></li>
+        <li><a href="link.html"><i class="fa fa-link"></i> Tautan</a></li>
+    </ul>
+
+    <div id="footer-bottom">
+        <p>Copyright © 2016 - PT. Transportasi Jakarta. All Rights Reserved.</p>
+    </div>
 </footer>
