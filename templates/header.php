76c267eed06b0b933451c4d34019cd552b370865
akito <akito@localhost.localdomain>
2016-02-24 08:02:35 +0700

    added primary navigation

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,44 @@
+
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+  <nav class="navbar yamm navbar-default navbar-fixed-top" role="navigation">
+  <?php 
+    // Fix menu overlap bug..
+    if ( is_admin_bar_showing() ) echo '<div style="min-height: 32px;"></div>'; 
+  ?>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/logo-tj-1.png" class="img-responsive" id="logo-tj" />
+      </a>
+
+      <ul class="nav navbar-nav" id="nav-burger">
+          <li>
+              <div class="burger-button">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </div>
+          </li>
+      </ul>
+      <form action="<?php home_url( '/' ); ?>" method="get"class="search-form" id="navbar-search" role="search">
+          <div class="form-group has-feedback">
+              <label for="search" class="sr-only">Search</label>
+              <input class="form-control" id="search" type="text" name="s" value="<?php the_search_query(); ?>">
+              <i class="fa fa-search form-control-feedback"></i>
+          </div>
+      </form> 
+
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        $walker = new Menu_With_Description;
+        wp_nav_menu( array( 'theme_location' => 'primary_navigation', 
+                            'container' => '', 
+                            'menu_class' => 'nav navbar-nav navbar-right', 
+                            'menu_id' => 'nav-header', 
+                            'walker' => $walker ) );
       endif;
       ?>
-    </nav>
-  </div>
+    </div>
+    <div class="container-fluid" id="header-bottom"></div>
+  </nav>
 </header>
