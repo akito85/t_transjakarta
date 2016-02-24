02284c9052a1f10f2f390eafed67778ca9f2a364
akito <akito@localhost.localdomain>
2016-02-24 08:33:46 +0700

    added footer dynamic

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,32 @@
 <footer class="container-fluid">
     <div id="footer-top"></div>
 
-    <ul class="nav" id="nav-footer">
-        <li><a href="contact.html"><i class="fa fa-comment"></i> Kontak Kami</a></li>
-        <li><a href="link.html"><i class="fa fa-link"></i> Tautan</a></li>
-    </ul>
+    <div class="nav" id="nav-footer">
+        <?php
+            $args = array( "category_name" => "footer-link", 
+                           "posts_per_page" => 6, 
+                           "post_type" => "post" );
+        
+            $posts = new WP_Query( $args );
+
+            // The Loop
+            if ( $posts->have_posts() ) {
+                while ( $posts->have_posts() ) {
+                    $posts->the_post();
+                    the_content(); 
+                }
+            } else {
+                // no posts found
+            }
+            /* Restore original Post Data */
+            wp_reset_postdata();            
+
+        ?>
+    </div>
 
     <div id="footer-bottom">
         <p>Copyright © 2016 - PT. Transportasi Jakarta. All Rights Reserved.</p>
     </div>
 </footer>
+
+
